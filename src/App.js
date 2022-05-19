85846909c60b305e89500ae3b088350e0a1851bf
SeanXiao705 <yuqixiao@usc.edu>
2022-05-18 19:31:25 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import { Layout } from "antd";
+import { Layout, Typography } from "antd";
+import { useState } from "react";
 import LoginForm from "./components/LoginForm";
 
 const { Header, Content } = Layout;
+const { Title } = Typography;
 
 function App() {
+  const [authed, setAuthed] = useState(false);
+
   return (
     <Layout style={{ height: "100vh" }}>
-      <Header>header</Header>
-      {/* <Content
+      <Header>
+        <div className="header">
+          <Title
+            level={2}
+            style={{ color: "white", lineHeight: "inherit", marginBottom: 0 }}
+          >
+            Lai Food
+          </Title>
+        </div>
+      </Header>
+      <Content
         style={{
           padding: "50px",
           maxHeight: "calc(100% - 64px)",
           overflowY: "auto",
         }}
       >
-        content
-      </Content> */}
-        <LoginForm />
+        {authed ? (
+          <div>content placeholder</div>
+        ) : (
+          <LoginForm onSuccess={() => setAuthed(true)} />
+        )}
+      </Content>
     </Layout>
   );
 }
